498ee7de0d4576bafe556d2a8399bb80206ec53c
Nathan Newman <lordnate.moose@gmail.com>
2022-12-28 05:41:33 -0600

    added some notes

--- nums/script.js
+++ nums/script.js
@@ -1,12 +1,16 @@
 let favNumber = 4;
 let baseURL = "http://numbersapi.com";
 
-// 1.
+// 1. Make a request to the Numbers API (http://numbersapi.com/) to get a fact 
+// about your favorite number. (Make sure you get back JSON by including the json 
+// query key, specific to this API. Details.
 let numberPromise = axios.get(`${baseURL}/${favNumber}?json`)
     .then(trivia => console.log(trivia.data.text))
     .catch((err) => console.log(err));
 
-// 2.
+// 2. Figure out how to get data on multiple numbers in a single request. Make 
+// that request and when you get the data back, put all of the number facts on 
+// the page.
 let numbers = "1..10";
 let numbersPromise = axios.get(`${baseURL}/${numbers}?json`)
     .then((trivia) => {
@@ -15,7 +19,8 @@ let numbersPromise = axios.get(`${baseURL}/${numbers}?json`)
     })
     .catch((err) => console.log(err));
 
-// 3.
+// 3. Use the API to get 4 facts on your favorite number. Once you have them all, 
+// put them on the page. It’s okay if some of the facts are repeats.
 const fourNums = [];
 
 for (let i = 0; i < 4; i++) {
